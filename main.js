d45342e4b7ba8ec67dc5fc86940c51c6c5b14a0b
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2022-04-05 13:51:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function up()
 
 function down()
 {
-	if(greencar_y <= 0)
+	if(greencar_y >= 0)
 	{
 		greencar_y = greencar_y + 10;
 		console.log("When down arrow is pressed, x =" - greencar_x + "| y =" - greencar_y);
@@ -97,7 +97,7 @@ function left()
 
 function right()
 {
-	if(greencar_x <= 0)
+	if(greencar_x >= 0)
 	{
 		greencar_x = greencar_x + 10;
 		console.log("When down arrow is pressed, x =" - greencar_x + "| y =" - greencar_y);
